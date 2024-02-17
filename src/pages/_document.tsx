f79241894c4b5affbe5828a54d320deae712e36b
Hitesh Pursani <pursani9@gmail.com>
2024-02-17 17:38:13 +0530

    removed error code

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,7 +6,6 @@ import {Head, Html, Main, NextScript} from 'next/document';
 //
 // next/document Head is rendered once on the server. This is different from next/head which will
 // rebuild the next/head fields each time it's called, and won't overwrite next/document's Head.
-
 export default function Document() {
   return (
     <Html lang="en">
